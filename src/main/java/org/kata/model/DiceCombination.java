c958943e015015dd68201a9c89010e32602199f2
sydney.adjou-moumouni <sydgael@gmail.com>
2024-04-22 10:12:41 +0200

    refactor yatzy score

--- src/main/java/org/kata/model/DiceCombination.java
+++ src/main/java/org/kata/model/DiceCombination.java
@@ -1,5 +1,9 @@
 package org.kata.model;
 
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public record DiceCombination(int d1, int d2, int d3, int d4, int d5) {
 
     public DiceCombination {
@@ -7,7 +11,20 @@ public record DiceCombination(int d1, int d2, int d3, int d4, int d5) {
             throw new IllegalArgumentException("Dice values cannot be less than 1.");
         }
     }
+
     public int sum() {
         return d1 + d2 + d3 + d4 + d5;
+    }
+
+    public int yatzy() {
+        var countingGroup = Stream.of(d1, d2, d3, d4, d5)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return countingGroup
+                .values()
+                .stream()
+                .filter(d -> d == 5)
+                .findFirst()
+                .map(e -> 50)
+                .orElse(0);
     }
 }
